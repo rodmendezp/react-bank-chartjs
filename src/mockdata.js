1617ba382775dca558470cc444099da0a4a419f7
Rodrigo Mendez <rodmendezp@gmail.com>
2021-01-24 15:18:13 -0300

    larger transaction data

--- src/mockdata.js
+++ src/mockdata.js
@@ -1,49 +1,338 @@
 export const jsonTransactions = [
     {
-        'int_amount': 17.33,
+        'int_amount': 49.47,
         'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-26T18:48:58'
+        'mail_dtime': '2019-10-09T12:06:41'
     },
     {
-        'int_amount': 8.5,
-        'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-22T11:53:09'
+        'amount': 3080.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-10-09T11:51:24'
     },
     {
-        'int_amount': 0.74,
-        'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-15T20:37:51'
+        'amount': 86000.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-08T16:16:59'
     },
     {
-        'int_amount': 13.95,
-        'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-14T06:52:16'
-    },
-    {
-        'int_amount': 14.96,
-        'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-11T19:37:47'
-    },
-    {
-        'int_amount': 1.48,
-        'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-07T21:57:48'
-    },
-    {
-        'int_amount': 6.85,
-        'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-03T23:34:10'
-    },
-    {
-        'amount': 145301.0,
-        'int_amount': 184.65,
-        'rate': 786.9,
+        'amount': 1076351.0,
+        'int_amount': 1478.2,
+        'rate': 728.15,
         'ttype': 'INT_CRED_PAY',
-        'mail_dtime': '2020-09-03T17:38:37'
+        'mail_dtime': '2019-10-07T21:41:47'
     },
     {
-        'int_amount': 7.78,
+        'amount': 1191.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-10-07T11:44:52'
+    },
+    {
+        'amount': 15000.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-10-07T10:24:03'
+    },
+    {
+        'int_amount': 1478.2,
         'ttype': 'INT_CRED_EXPENSE',
-        'mail_dtime': '2020-09-02T13:11:26'
+        'mail_dtime': '2019-10-07T08:48:51'
+    },
+    {
+        'amount': 62400.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-06T23:56:20'
+    },
+    {
+        'amount': 5381.0,
+        'int_amount': 7.42,
+        'rate': 725.15,
+        'ttype': 'INT_CRED_PAY',
+        'mail_dtime': '2019-10-06T20:17:20'
+    },
+    {
+        'amount': 171100.0,
+        'ttype': 'NAT_CRED_PAY',
+        'mail_dtime': '2019-10-06T20:16:06'
+    },
+    {
+        'amount': 120436.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-06T20:10:36'
+    },
+    {
+        'amount': 6110.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-06T20:06:53'
+    },
+    {
+        'amount': 34465.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-06T20:06:46'
+    },
+    {
+        'amount': 1650.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-05T18:29:26'
+    },
+    {
+        'amount': 1899.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-05T15:37:03'
+    },
+    {
+        'amount': 5000.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-05T15:24:26'
+    },
+    {
+        'int_amount': 7.28,
+        'ttype': 'INT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-03T23:34:09'
+    },
+    {
+        'amount': 1540.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-10-01T13:11:08'
+    },
+    {
+        'amount': 30000.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-09-30T22:15:55'
+    },
+    {
+        'amount': 1400.0,
+        'ttype': 'CHECK_EXPENSE',
+        'mail_dtime': '2019-09-30T19:50:39'
+    },
+    {
+        'amount': 147588.0,
+        'ttype': 'NAT_CRED_PAY',
+        'mail_dtime': '2019-09-30T18:42:31'
+    },
+    {
+        'amount': 1990.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-30T12:48:07'
+    },
+    {
+        'amount': 1500.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-30T07:03:09'
+    },
+    {
+        'amount': 5000.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-09-29T15:31:27'
+    },
+    {
+        'amount': 61816.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-29T15:21:05'
+    },
+    {
+        'amount': 6700.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-28T23:33:11'
+    },
+    {
+        'amount': 16170.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-28T18:34:48'
+    },
+    {
+        'amount': 1100.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-28T08:32:36'
+    },
+    {
+        'amount': 4990.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-27T20:25:35'
+    },
+    {
+        'amount': 5000.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-27T17:53:11'
+    },
+    {
+        'amount': 7150.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-27T13:38:33'
+    },
+    {
+        'amount': 31350.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-26T21:57:42'
+    },
+    {
+        'amount': 5100.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-26T13:49:30'
+    },
+    {
+        'amount': 1390.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-25T13:36:58'
+    },
+    {
+        'amount': 22500.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-09-25T10:54:35'
+    },
+    {
+        'amount': 145322.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-09-24T18:41:27'
+    },
+    {
+        'amount': 1650.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-24T13:29:10'
+    },
+    {
+        'amount': 16725.0,
+        'int_amount': 22.84,
+        'rate': 732.25,
+        'ttype': 'INT_CRED_PAY',
+        'mail_dtime': '2019-09-24T03:10:54'
+    },
+    {
+        'amount': 58006.0,
+        'ttype': 'NAT_CRED_PAY',
+        'mail_dtime': '2019-09-24T03:10:12'
+    },
+    {
+        'amount': 14990.0,
+        'ttype': 'CHECK_EXPENSE',
+        'mail_dtime': '2019-09-24T03:06:08'
+    },
+    {
+        'amount': 6490.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-23T11:44:46'
+    },
+    {
+        'amount': 10986.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-23T11:38:22'
+    },
+    {
+        'amount': 8180.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-22T13:56:02'
+    },
+    {
+        'amount': 257.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-22T13:55:31'
+    },
+    {
+        'int_amount': 7.74,
+        'ttype': 'INT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-21T20:54:24'
+    },
+    {
+        'amount': 1400.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-21T20:28:06'
+    },
+    {
+        'amount': 7300.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-21T20:26:54'
+    },
+    {
+        'int_amount': 4.88,
+        'ttype': 'INT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-17T23:11:02'
+    },
+    {
+        'int_amount': 4.88,
+        'ttype': 'INT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-17T23:08:18'
+    },
+    {
+        'amount': 23650.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-17T23:04:10'
+    },
+    {
+        'int_amount': 9.61,
+        'ttype': 'INT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-16T20:26:44'
+    },
+    {
+        'amount': 193340.0,
+        'ttype': 'NAT_CRED_PAY',
+        'mail_dtime': '2019-09-16T19:33:22'
+    },
+    {
+        'amount': 10000.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-16T16:51:09'
+    },
+    {
+        'amount': 13350.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-14T23:04:45'
+    },
+    {
+        'amount': 92790.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-14T13:32:34'
+    },
+    {
+        'amount': 19618.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-14T11:54:17'
+    },
+    {
+        'amount': 20002.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-14T01:30:46'
+    },
+    {
+        'amount': 4280.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-13T20:49:25'
+    },
+    {
+        'amount': 4900.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-12T13:06:51'
+    },
+    {
+        'amount': 8400.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-12T02:01:18'
+    },
+    {
+        'amount': 20000.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-12T01:55:25'
+    },
+    {
+        'amount': 39323.0,
+        'ttype': 'NAT_CRED_PAY',
+        'mail_dtime': '2019-09-11T07:54:12'
+    },
+    {
+        'amount': 6000.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-11T07:35:00'
+    },
+    {
+        'amount': 15159.0,
+        'ttype': 'CHECK_EXPENSE',
+        'mail_dtime': '2019-09-10T17:33:17'
+    },
+    {
+        'amount': 1300.0,
+        'ttype': 'NAT_CRED_EXPENSE',
+        'mail_dtime': '2019-09-10T13:58:49'
+    },
+    {
+        'amount': 90000.0,
+        'ttype': 'CHECK_TRANSFER',
+        'mail_dtime': '2019-09-10T10:22:49'
     }
 ]
